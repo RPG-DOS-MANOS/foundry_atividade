18d415407da50ea9903d15ed4a3c432cce4f366b
ari123rm <141646190+ari123rm@users.noreply.github.com>
2025-08-02 20:11:05 -0300

    reararara

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FoundryVTTViewer from './components/FoundryVTTViewer';
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <FoundryVTTViewer foundryUrl="/" />
+      
+      <BrowserRouter>
+      {/* <Header/> */}
+      <div className='rota'>
+        <Routes>
+          <Route path='/app/*' element={<FoundryVTTViewer foundryUrl="/" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+
     </div>
   );
 }
